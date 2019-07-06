efab892043531059969126ba0e4d3a4497880fdc
alexandrrusanov <Darkelf3001>
2019-07-06 16:30:56 +0300

    js hw15 done

--- js/script.js
+++ js/script.js
@@ -16,8 +16,6 @@ const changeThemeBtn = document.getElementById("changeThemeBtn"),
 
 let colorTheme = window.localStorage.getItem("colorTheme");
 
-console.log(colorTheme);
-
 switch (colorTheme) {
     case "green":
         sectionHeader.forEach((elem) => {
@@ -35,3 +33,34 @@ switch (colorTheme) {
 changeThemeBtn.onclick = (event) => {
     toggleSectionHeaderTheme();
 };
+
+//Anchors smooth scrolling
+$(document).ready(function(){
+    $(".header-menu-item").on('click', function(event) {
+        $('html').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 800)
+    });
+});
+
+//Btn to top
+const $btn = $('.btn-to-top');
+
+$(document).scroll(function() {
+
+    let $heightClient = $(window).innerHeight();
+
+    ($(window).scrollTop() > $heightClient) ? $('.btn-to-top').fadeIn('slow'): $('.btn-to-top').fadeOut('slow');
+});
+
+$btn.click(function() {
+    $('html').animate({scrollTop: 0}, 800)
+});
+
+//slideToggle Hot News
+$(".btn-toggle-news").html("Hide news");
+const btnToggleNews = document.querySelector(".btn-toggle-news");
+$(".btn-toggle-news").click(function() {
+    $("#section4").slideToggle("slow");
+    (btnToggleNews.innerText === "Hide news") ? btnToggleNews.innerText = "Show news": btnToggleNews.innerText = "Hide news";
+});
